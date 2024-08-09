5435bed84906f59d61eaa7e46ab4c309f81423e9
kimestelle <137699667+kimestelle@users.noreply.github.com>
2024-08-09 16:00:25 -0700

    new file path

--- server/app.py
+++ server/app.py
@@ -1,15 +1,23 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
-import random
-from .get_next_word import get_next_word
+import os
+from get_next_word import get_next_word
 
 # instantiate the app
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/dist')
 app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+# Serve the frontend
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/next_word', methods=['POST'])
 def next_word():
